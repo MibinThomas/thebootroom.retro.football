0c96e1305f212fe4b0ac83bbcfdd474ac540b223
Mibin Thomas <mibinthomas22@gmail.com>
2026-01-07 14:18:26 +0400

    logo added to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["pdfkit", "fontkit"]
-  }
+    serverComponentsExternalPackages: ["pdfkit", "fontkit"],
+
+    // ✅ Ensure the logo file is included in the serverless bundle
+    outputFileTracingIncludes: {
+      // include for your route bundle
+      "/app/api/teams/**": ["./public/logo.png"],
+    },
+  },
 };
 
 module.exports = nextConfig;
